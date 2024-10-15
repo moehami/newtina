03663ea3f238f2026d8e2d3f0aedaef26bd28995
joshbermanssw <joshberman@ssw.com.au>
2024-10-16 10:39:33 +1100

    fix pricing component

--- components/shared/Blocks/Pricing.tsx
+++ components/shared/Blocks/Pricing.tsx
@@ -39,7 +39,7 @@ const Pricing = ({ data }: PricingProps) => {
   const { title, description, allPlans, plans } = data;
 
   return (
-    <div className="pricing-component container mx-auto p-4 lg:pb-40">
+    <div className="pricing-component container mx-auto p-4 mt-20 md:mt-0 lg:pb-40">
       {title && (
         <h1
           className="text-5xl text-center  text-white mb-4"
@@ -58,7 +58,7 @@ const Pricing = ({ data }: PricingProps) => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-5 px-28 lg:px-12">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 xl:grid-cols-5 px-12 lg:px-12">
         {allPlans && allPlans.length > 0 && (
           <div
             className="all-plans p-4 rounded-lg text-white col-span-1 lg:col-span-2 lg:mx-auto xl:col-span-1"
